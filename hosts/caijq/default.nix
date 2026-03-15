1c324654c2f7d4168005e94f6a5c0aee1357fe86
caijq00 <caijq@qq.com>
2026-03-15 23:36:13 +0800

    feat: 新增 Dockge 声明式模块并在主机配置启用

--- hosts/caijq/default.nix
+++ hosts/caijq/default.nix
@@ -8,6 +8,7 @@
     ./security.nix
     ../../modules/nixos/shell.nix
     ../../modules/nixos/docker.nix
+    ../../modules/compose/dockge.nix
     # ../../modules/nixos/incus.nix
     ../../modules/nixos/sing-box.nix
     # ../../modules/nixos/emby.nix
@@ -16,6 +17,9 @@
   ];
 
   networking.hostName = hostName;
+  # Dockge is managed declaratively via the NixOS module above.
+  # Do not manage Dockge through compose/dockge/docker-compose.yml.
+  my.services.dockge.openFirewall = true;
 
   boot.loader.grub.enable = true;
   boot.loader.grub.devices = [ "/dev/vda" ];
---
+++ modules/compose/dockge.nix
@@ -0,0 +1,60 @@
+{ lib, config, ... }:
+
+let
+  cfg = config.my.services.dockge;
+in
+{
+  options.my.services.dockge = {
+    enable = lib.mkEnableOption "Dockge" // {
+      default = true;
+    };
+
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 5001;
+      description = "Host port Dockge listens on (used for firewall only).";
+    };
+
+    stacksDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/etc/nixos/nixos-vps/compose";
+      description = "Directory containing docker compose stacks.";
+    };
+
+    dataDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/etc/nixos/nixos-vps/compose/dockge";
+      description = "Dockge data directory.";
+    };
+
+    openFirewall = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to open the firewall for the Dockge port.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    virtualisation.oci-containers.backend = "docker";
+
+    systemd.tmpfiles.rules = [
+      "d ${cfg.dataDir} 0755 root root -"
+    ];
+
+    virtualisation.oci-containers.containers.dockge = {
+      image = "louislam/dockge:latest";
+      autoStart = true;
+      extraOptions = [ "--network=host" ];
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock"
+        "${cfg.stacksDir}:/opt/stacks"
+        "${cfg.dataDir}:/app/data"
+      ];
+      environment = {
+        DOCKGE_STACKS_DIR = "/opt/stacks";
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ cfg.port ];
+  };
+}
